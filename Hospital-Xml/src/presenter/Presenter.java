bc3b087f443f17bb2562ee23af35f94180f2bd48
DavidSan <84092328+DavidSan9896@users.noreply.github.com>
2023-04-10 22:19:23 -0500

    Add files via upload

---
+++ Hospital-Xml/src/presenter/Presenter.java
@@ -0,0 +1,54 @@
+package presenter;
+
+import view.View;
+
+public class Presenter {
+    private View view;
+
+    public Presenter() {
+        view = new View();
+    }
+
+    public static void main(String[] args) {
+        Presenter presenter = new Presenter();
+        presenter.run();
+    }
+
+    private void run() {
+        try {
+            int option = 0;
+            do {
+                switch (option){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        View.print("Hasta Luego");
+                        break;
+                    default:
+                }
+            }while ((option = view.menu())!= 5 );
+        }catch (NumberFormatException e){
+            View.print("Solo se permiten Valores Numericos");
+        }
+    }
+
+    private void addRoom() {
+        int id = view.readIntegers("Ingrese el id de la habitacion: ");
+        int roomNumbre = view.readIntegers("Ingrese el numero de piso de la habitacion: ");
+        int floorNumbers = view.readIntegers("Ingrese el numero de la habitacion: ");
+        int bedNumbers =  view.readIntegers("Ingrese el numero de camas de la habitación: ");
+    }
+
+    private void addPatient() {
+        view.print("Se debe crear  una habitacion");
+        int id = view.readIntegers("Ingrese el id de la habitacion qeu se le asiganara: ");
+        view.readString("Ingrese el nombre : ");
+        view.readString("Ingrese el apellido : ");
+    }
+}
